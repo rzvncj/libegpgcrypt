eacf0c6f11b6e97f780d21a2dae1d67e94e4f540
Razvan Cojocaru <rzvncj@gmail.com>
2021-01-25 19:56:17 +0200

    Remove superfluous scope from memory_data_buffer

--- lib/memory_data_buffer.cpp
+++ lib/memory_data_buffer.cpp
@@ -10,10 +10,9 @@ memory_data_buffer::memory_data_buffer()
 {
     gpgme_error_t ret = gpgme_data_new(&data_);
 
-    if(ret != GPG_ERR_NO_ERROR) {
+    if(ret != GPG_ERR_NO_ERROR)
         throw std::runtime_error(std::string("file data buffer creation failed: ")
             + egpg_gpgme_strerror(ret));
-    }
 }
 
 
@@ -22,11 +21,10 @@ memory_data_buffer::memory_data_buffer(const std::string& data)
     gpgme_error_t ret =
         gpgme_data_new_from_mem(&data_, data.c_str(), data.length(), 1);
 
-    if(ret != GPG_ERR_NO_ERROR) {
+    if(ret != GPG_ERR_NO_ERROR)
         throw std::runtime_error(
             std::string("file data buffer creation failed: ")
             + egpg_gpgme_strerror(ret));
-    }
 }
 
 
